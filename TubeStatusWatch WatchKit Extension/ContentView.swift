436aa046242beed8627dc17a1050e23785508cf2
Dylan Maryk <dylan@maryk.uk>
2020-06-27 20:37:09 +0200

    Initial Commit

---
+++ TubeStatusWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TubeStatusWatch WatchKit Extension
+//
+//  Created by Dylan Maryk on 27/06/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TubeStatusWatch WatchKit Extension/TubeStatusWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  TubeStatusWatchApp.swift
+//  TubeStatusWatch WatchKit Extension
+//
+//  Created by Dylan Maryk on 27/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct TubeStatusWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
