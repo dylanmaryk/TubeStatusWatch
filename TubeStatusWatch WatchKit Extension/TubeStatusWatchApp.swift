0ae55d4b291812b6f9dc313a4280db967b81922d
Dylan Maryk <dylan@maryk.uk>
2020-08-01 20:47:18 +0200

    Make lines selectable

--- TubeStatusWatch WatchKit Extension/TubeStatusWatchApp.swift
+++ TubeStatusWatch WatchKit Extension/TubeStatusWatchApp.swift
@@ -5,40 +5,66 @@
 //  Created by Dylan Maryk on 27/06/2020.
 //
 
+import Combine
 import SwiftUI
 
-struct LineSetting: Codable, Identifiable {
+class LineSetting: Codable, Identifiable, ObservableObject {
+    enum CodingKeys: CodingKey {
+        case id
+        case name
+        case isSelected
+    }
+    
     let id: String
     let name: String
+    @Published var isSelected: Bool
+    
+    init(id: String, name: String, isSelected: Bool) {
+        self.id = id
+        self.name = name
+        self.isSelected = isSelected
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        isSelected = try container.decode(Bool.self, forKey: .isSelected)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(isSelected, forKey: .isSelected)
+    }
+}
+
+struct LineButton: View {
+    @ObservedObject var lineSetting: LineSetting
+    @Binding var selectedLineSettingIds: [String]
+    
+    var body: some View {
+        Button(lineSetting.name) {
+            lineSetting.isSelected.toggle()
+            lineSetting.isSelected
+                ? selectedLineSettingIds.append(lineSetting.id)
+                : selectedLineSettingIds.removeAll { $0 == lineSetting.id }
+        }
+//        .listRowBackground(Color(lineSetting.id)
+        .listRowBackground((lineSetting.isSelected ? Color.green : Color.red) // temp
+                            .cornerRadius(9))
+    }
 }
 
 struct LineList: View {
-    private static let defaultLineSettings = [LineSetting(id: "bakerloo", name: "Bakerloo"),
-                                              LineSetting(id: "central", name: "Central"),
-                                              LineSetting(id: "circle", name: "Circle"),
-                                              LineSetting(id: "district", name: "District"),
-                                              LineSetting(id: "dlr", name: "DLR"),
-                                              LineSetting(id: "hammersmith-city", name: "Hammersmith & City"),
-                                              LineSetting(id: "jubilee", name: "Jubilee"),
-                                              LineSetting(id: "london-overground", name: "London Overground"),
-                                              LineSetting(id: "metropolitan", name: "Metropolitan"),
-                                              LineSetting(id: "northern", name: "Northern"),
-                                              LineSetting(id: "piccadilly", name: "Piccadilly"),
-                                              LineSetting(id: "tfl-rail", name: "TfL Rail"),
-                                              LineSetting(id: "tram", name: "Tram"),
-                                              LineSetting(id: "victoria", name: "Victoria"),
-                                              LineSetting(id: "waterloo-city", name: "Waterloo & City")]
-    private static let defaultLineSettingsData = try! JSONEncoder().encode(Self.defaultLineSettings)
-    
-    @AppStorage(wrappedValue: Self.defaultLineSettingsData, "lineSettings") var lineSettingsData
+    let lineSettings: [LineSetting]
+    @Binding var selectedLineSettingIds: [String]
     
     var body: some View {
         List {
-            let lineSettings = try! JSONDecoder().decode([LineSetting].self, from: lineSettingsData)
             ForEach(lineSettings) { lineSetting in
-                    Text(lineSetting.name)
-                        .listRowBackground(Color(lineSetting.id)
-                                            .cornerRadius(9))
+                LineButton(lineSetting: lineSetting, selectedLineSettingIds: $selectedLineSettingIds)
             }
         }
     }
@@ -46,15 +72,65 @@ struct LineList: View {
 
 @main
 struct TubeStatusWatchApp: App {
+    @AppStorage("selectedLineSettingIds") var selectedLineSettingIdsString = ""
+    
+    private static let lineIds = ["bakerloo",
+                                  "central",
+                                  "circle",
+                                  "district",
+                                  "dlr",
+                                  "hammersmith-city",
+                                  "jubilee",
+                                  "london-overground",
+                                  "metropolitan",
+                                  "northern",
+                                  "piccadilly",
+                                  "tfl-rail",
+                                  "tram",
+                                  "victoria",
+                                  "waterloo-city"]
+    
+    private static let lineNames = ["Bakerloo",
+                                    "Central",
+                                    "Circle",
+                                    "District",
+                                    "DLR",
+                                    "Hammersmith & City",
+                                    "Jubilee",
+                                    "London Overground",
+                                    "Metropolitan",
+                                    "Northern",
+                                    "Piccadilly",
+                                    "TfL Rail",
+                                    "Tram",
+                                    "Victoria",
+                                    "Waterloo & City"]
+    
     var body: some Scene {
         WindowGroup {
-            LineList()
+            let selectedLineSettingIds = Binding(
+                get: { selectedLineSettingIdsString.components(separatedBy: ",") },
+                set: { selectedLineSettingIdsString = $0.joined(separator: ",") }
+            )
+            let lineSettings = Self.lineIds.enumerated().map { index, lineId in
+                LineSetting(id: lineId,
+                            name: Self.lineNames[index],
+                            isSelected: selectedLineSettingIds.wrappedValue.contains(lineId))
+            }
+            LineList(lineSettings: lineSettings, selectedLineSettingIds: selectedLineSettingIds)
         }
     }
 }
 
 struct TubeStatusWatchApp_Previews: PreviewProvider {
     static var previews: some View {
-        LineList()
+        let lineSettings = [LineSetting(id: "bakerloo", name: "Bakerloo", isSelected: true),
+                            LineSetting(id: "central", name: "Central", isSelected: false),
+                            LineSetting(id: "circle", name: "Circle", isSelected: false)]
+        let selectedLineSettingIds = Binding(
+            get: { ["bakerloo"] },
+            set: { _ in }
+        )
+        LineList(lineSettings: lineSettings, selectedLineSettingIds: selectedLineSettingIds)
     }
 }
